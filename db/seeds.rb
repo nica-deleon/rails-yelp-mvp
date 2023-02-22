3e651a350b843bf56c42dcd8c4253957fa534538
nica de leonÃ <vdeleon0000@gmail.com>
2023-02-22 14:36:11 -0300

    listo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+10.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Dessert.variety,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+
+  3.times do
+    Review.create!(
+      content: Faker::HowIMetYourMother.quote,
+      rating: %w(0 1 2 3 4 5).sample,
+      restaurant_id: restaurant.id
+    )
+  end
+end
